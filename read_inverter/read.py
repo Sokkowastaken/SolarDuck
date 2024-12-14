c3d4352f40ec5f47b7c76ec803b17836a2a37345
sokkowastaken <xjoosx@hotmail.nl>
2024-12-14 11:28:01 +0100

    Adjusted inverter commands, tried different way of reading and added reading option underneath main

--- read_inverter/read.py
+++ read_inverter/read.py
@@ -1,8 +1,8 @@
 import serial
 import time
 
-#windows = "COM3" linux = /dev/usbttyx
-PORT = 'COM3'
+#windows = "COMx" linux = /dev/usbttyx
+PORT = 'COM6'
 BAUDRATE = 9600  
 
 # Open the serial port
@@ -11,11 +11,13 @@ ser = serial.Serial(
     baudrate=BAUDRATE,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS
+    bytesize=serial.EIGHTBITS,
+    timeout=1 
 )
+#CHECK INVERTER ADDRESS x7E
+inverter_command = b'\x3F\x23\x0B\x34\x41\x0B\x32\x59\x31\x35\x30\x30\x23\x3F'
+inverter_command_2 = b'\x3F\x23\x0B\x34\x42\x0B\x23\x3F'
 
-inverter_command = b'\x3F\x23\x7E\x34\x41\x7E\x32\x59\x31\x30\x30\x23\x3F'
-inverter_command_2 = b'\x3F\x23\x7E\x34\x42\x7E\x23\x3F'
 if ser.is_open:
     print(f"Serial port {PORT} opened successfully")
 
@@ -24,8 +26,8 @@ def send_command(command):
     print(f"Sent: {command}")
 
 def read_response():
-    response = ser.read(100)
-    print(f"Received: {response}\n")
+    response = ser.read(30)
+    print(f"Received: {response}")
     return response
 
 
@@ -33,14 +35,39 @@ def main():
     print("listening for data")
     while True:
         read_response()
-        #sleeping for 1.5 seconds sleeptime based on the value filled in by inverter command.
         time.sleep(1.5) 
     
 if __name__ == "__main__":
-    send_command(inverter_command)
-    time.sleep(1.5)
-    read_response()
-    time.sleep(0.5)
-    send_command(inverter_command_2)
-    time.sleep(1.5)
-    main()
+    try:
+        send_command(inverter_command)
+        time.sleep(1)
+        read_response()
+        send_command(inverter_command_2)
+        time.sleep(1.5)
+        main()
+    
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+# import serial
+# import time
+
+# ser = serial.Serial('COM3', 9600, timeout=1)
+# time.sleep(2)  
+
+# # File to save the data
+# output_file = "arduino_output.txt"
+
+# with open(output_file, "w") as file:
+#     print("Logging data... Press Ctrl+C to stop.")
+#     try:
+#         while True:
+#             if ser.in_waiting > 0:
+#                 line = ser.readline().decode('utf-8').strip()
+#                 print(line)  # Print to console
+#                 file.write(line + "\n")  # Write to file
+#     except KeyboardInterrupt:
+#         print("\nLogging stopped.")
+#     finally:
+#         ser.close()
